face94db46bab1fa78e7eb448eda0af03dc0375c
Santiago Siri <santiagosiri2001@gmail.com>
2023-06-08 16:44:46 -0300

    actualizacion de proyecto evolutivo tema 5

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-// import TaskListComponent from './components/containers/taskList';
-import ComponenteA from './components/containers/ComponenteA';
+import TaskListComponent from './components/containers/taskList';
+// import ComponenteA from './components/containers/ComponenteA';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* <TaskListComponent></TaskListComponent> */}
-        <ComponenteA></ComponenteA>
+        <TaskListComponent></TaskListComponent>
+        {/* <ComponenteA></ComponenteA> */}
       </header>
     </div>
   );
--- src/components/containers/taskList.jsx
+++ src/components/containers/taskList.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect , useState} from 'react'
 import { Task } from '../../models/task.class';
 import { LEVELS } from '../../models/levels.enum';
 import TaskComponent from '../pure/task';
@@ -6,6 +6,19 @@ import TaskComponent from '../pure/task';
 const TaskListComponent = () => {
 
   const defaultTask = new Task('example','default description',false ,LEVELS.NORMAL)
+
+  const [tasks, setTaks] = useState([defaultTask]);
+
+  useEffect(() => {
+    console.log('task has been modified')
+    return () => {
+      console.log('tasklist component is going to unmount...')
+    };
+  }, [tasks]);
+
+  const changeCompleted = (id) => {
+    console.log('TODO: cambiar el estado de una task')
+  }
 
   return (
     <div>
